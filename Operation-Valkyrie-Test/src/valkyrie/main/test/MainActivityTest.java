b0e46a319d799b477721e2a21196902efb7e5512
MiloTischler <milo.tischler@gmail.com>
2012-04-11 14:00:42 +0200

    Added src folder for test project

---
+++ Operation-Valkyrie-Test/src/valkyrie/main/test/MainActivityTest.java
@@ -0,0 +1,10 @@
+package valkyrie.main.test;
+
+import valkyrie.main.MainActivity;
+import android.test.ActivityInstrumentationTestCase2;
+
+public class MainActivityTest extends android.test.ActivityInstrumentationTestCase2<MainActivity> {
+    public MainActivityTest() {
+        super("com.example.helloandroid", MainActivity.class);
+     }
+}
