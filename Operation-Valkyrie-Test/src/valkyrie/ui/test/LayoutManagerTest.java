6ed034918fcdfd265156f46003b3e905d191f170
Jakob Schweighofer <theschweigi@gmail.com>
2012-04-12 23:16:02 +0200

    Added LayoutManagerTest Class and modified .gitignore

---
+++ Operation-Valkyrie-Test/src/valkyrie/ui/test/LayoutManagerTest.java
@@ -0,0 +1,22 @@
+package valkyrie.ui.test;
+
+import junit.framework.TestCase;
+
+/**
+ * 
+ * COPYRIGHT: Paul Neuhold, Laurenz Theuerkauf, Alexander Ritz, Jakob Schweighofer, Milo Tischler
+ * © Milo Tischler, Jakob Schweighofer, Alexander Ritz, Paul Neuhold, Laurenz Theuerkauf 
+ *
+ */
+public class LayoutManagerTest extends TestCase {
+	
+	
+	public LayoutManagerTest() {
+	}
+	
+	@Override
+	protected void setUp() {
+		
+	}
+	
+}
