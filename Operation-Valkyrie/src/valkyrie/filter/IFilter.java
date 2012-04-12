af33021a313c6d54fe1ad60143807846648abd98
MiloTischler <milo.tischler@gmail.com>
2012-04-12 17:30:43 +0200

    Implemented FilterManager and Filter Skeletons

--- Operation-Valkyrie/src/valkyrie/filter/IFilter.java
+++ Operation-Valkyrie/src/valkyrie/filter/IFilter.java
@@ -1,5 +1,11 @@
 package valkyrie.filter;
 
-public interface IFilter {
+import java.util.Vector;
 
+import android.graphics.Bitmap;
+
+public interface IFilter {
+	public void manipulatePreviewImage(Bitmap bitmap);
+	public void manipulateImage(Bitmap bitmap);
+	public Vector<Object> getUIElements();
 }
