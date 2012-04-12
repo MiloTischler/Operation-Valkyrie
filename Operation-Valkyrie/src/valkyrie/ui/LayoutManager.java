11c29059814da012ac12220fd2fd2eed077db3ca
Jakob Schweighofer <theschweigi@gmail.com>
2012-04-12 21:05:03 +0200

    Basic LayoutManager communication and UI Components registration

--- Operation-Valkyrie/src/valkyrie/ui/LayoutManager.java
+++ Operation-Valkyrie/src/valkyrie/ui/LayoutManager.java
@@ -1,7 +1,43 @@
 package valkyrie.ui;
 
+import android.view.View;
+
 public class LayoutManager {
-	private LayoutManager instance = null;
+	/**
+	 * @todo Jakob
+	 */
+	private static LayoutManager instance = null;
+
+	/**
+	 * Default-Konstruktor, der nicht außerhalb dieser Klasse aufgerufen werden
+	 * kann.
+	 */
+	private LayoutManager() {
+	}
+
+	/**
+	 * Statische Methode, liefert die einzige Instanz dieser Klasse zurück
+	 */
+	public static LayoutManager getInstance() {
+
+		if (instance == null) {
+			instance = new LayoutManager();
+		}
+
+		return instance;
+	}
 	
+	/**
+	 * @todo Jakob
+	 */
+	public void notifyUI(Object filterObject) {
+		
+	}
 	
+	/**
+	 * Registers the main layout.
+	 */
+	public void setContentView(View mainView) {
+		
+	}
 }
--- Operation-Valkyrie/src/valkyrie/ui/MainActivity.java
+++ Operation-Valkyrie/src/valkyrie/ui/MainActivity.java
@@ -5,12 +5,17 @@ import android.app.Activity;
 import android.os.Bundle;
 
 public class MainActivity extends Activity {
-	
-    /** Called when the activity is first created. */
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
-    }
-    
+
+	/** Called when the activity is first created. */
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.main);
+
+		// register UI components, all at once
+		LayoutManager.getInstance().setContentView(this.findViewById(R.layout.main));
+		
+	}
+
 }
