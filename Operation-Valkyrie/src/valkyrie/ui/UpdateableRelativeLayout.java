04051098b5d0f805badf873a1316ff650b50f59d
Jakob Schweighofer <theschweigi@gmail.com>
2012-04-12 21:08:46 +0200

    Added Relative Layout Derivation

---
+++ Operation-Valkyrie/src/valkyrie/ui/UpdateableRelativeLayout.java
@@ -0,0 +1,17 @@
+package valkyrie.ui;
+
+import android.content.Context;
+import android.widget.RelativeLayout;
+
+public class UpdateableRelativeLayout extends RelativeLayout implements IUpdateableUI {
+
+	public UpdateableRelativeLayout(Context context) {
+		super(context);
+	}
+
+	public void redrawUI() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
