f8e9529791b7a3b65081e1bfdddb6e8ff428f793
MiloTischler <milo.tischler@gmail.com>
2012-04-13 02:04:15 +0200

    Delted res array for filters from Testproject

--- Operation-Valkyrie-Test/gen/valkyrie/main/test/R.java
+++ Operation-Valkyrie-Test/gen/valkyrie/main/test/R.java
@@ -8,9 +8,6 @@
 package valkyrie.main.test;
 
 public final class R {
-    public static final class array {
-        public static final int filters=0x7f040000;
-    }
     public static final class attr {
     }
     public static final class drawable {
@@ -20,7 +17,7 @@ public final class R {
         public static final int main=0x7f030000;
     }
     public static final class string {
-        public static final int app_name=0x7f050001;
-        public static final int hello=0x7f050000;
+        public static final int app_name=0x7f040001;
+        public static final int hello=0x7f040000;
     }
 }
