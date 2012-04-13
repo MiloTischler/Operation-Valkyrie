aa1195c0c13b58fb7c9ab8103fce0ff92317fbcf
Paul Neuhold <paul_neuhold@hotmail.com>
2012-04-13 14:32:00 +0200

    eigentlich kein commit aber git wills was solls xD

--- Operation-Valkyrie/src/valkyrie/ui/MainActivity.java
+++ Operation-Valkyrie/src/valkyrie/ui/MainActivity.java
@@ -4,13 +4,24 @@ import valkyrie.main.R;
 import android.app.Activity;
 import android.os.Bundle;
 
+/**
+ * 
+ * COPYRIGHT: Paul Neuhold, Laurenz Theuerkauf, Alexander Ritz, Jakob
+ * Schweighofer, Milo Tischler © Milo Tischler, Jakob Schweighofer, Alexander
+ * Ritz, Paul Neuhold, Laurenz Theuerkauf
+ * 
+ */
 public class MainActivity extends Activity {
-	
-    /** Called when the activity is first created. */
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
-    }
-    
+
+	/** Called when the activity is first created. */
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.main);
+
+		// register UI components, all at once
+		// LayoutManager.getInstance().setMainActivity(this);
+	}
+
 }
