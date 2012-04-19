be603b68c274a65adeb0cf002b3983524c2cb402
Aexander Ritz <alexander.ritz@gmx.at>
2012-04-19 08:18:31 +0200

    Bitte jetzt richtig: die ersten gehversuche des filters:D

--- Operation-Valkyrie/src/valkyrie/filter/ascii/Ascii.java
+++ Operation-Valkyrie/src/valkyrie/filter/ascii/Ascii.java
@@ -1,9 +1,13 @@
 package valkyrie.filter.ascii;
 
+import java.io.BufferedInputStream;
+import java.io.FileInputStream;
 import java.util.Vector;
 
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.graphics.Color;
+import android.os.Environment;
 import android.util.Log;
 
 import valkyrie.filter.FilterAssets;
@@ -18,8 +22,7 @@ import valkyrie.filter.IFilter;
 public class Ascii implements IFilter {
 	
 	private Bitmap bm;
-	
-	
+	private Font activeFont;	
 	private Converter converter;
 	
 	public Ascii(){
@@ -30,15 +33,38 @@ public class Ascii implements IFilter {
 			if((i % 10) == 0)
 				colors[i] = Color.RED;
 		}
-		this.bm = Bitmap.createBitmap(colors,10, 10, Bitmap.Config.RGB_565);
+		//this.bm = Bitmap.createBitmap(colors,10, 10, Bitmap.Config.RGB_565);
+        
+
+        FileInputStream in;
+        BufferedInputStream buf;
+        String path = Environment.getExternalStorageDirectory().toString();
+        try {
+       	    in = new FileInputStream( path + "/oruxmaps/cursors/neodraig2.png");
+            buf = new BufferedInputStream(in);
+            this.bm = BitmapFactory.decodeStream(buf);
+            if (in != null) {
+         	in.close();
+            }
+            if (buf != null) {
+         	buf.close();
+            }
+        } catch (Exception e) {
+            Log.e("Error reading file", e.toString());
+        }
+		
+		
+		
 		Log.d("valkyrie", this.bm.getPixel(1, 1) + " lol");
 		
 		this.fonts = new Vector<Font>();
 		for (String name : this.fontsList) {
-			this.fonts.add(new Font(name));
-		}	
+			this.fonts.add(new Font(name, true));
+		}
 		
-		this.converter = new Converter(this.fonts.get(0).getLUT());
+		this.activeFont = this.fonts.get(0);
+		
+		this.converter = new Converter();
 		manipulatePreviewImage(this.bm);
 		
 	
@@ -47,12 +73,12 @@ public class Ascii implements IFilter {
 
 	public void manipulatePreviewImage(Bitmap bitmap) {
 		Bitmap bm2 = this.converter.bitmapToGrayScale(this.bm);
-		Log.d("valkyrie",  bm2.getPixel(0, 0) +  " lol2");	
-		this.converter.asciiTextToImage(this.converter.grayScaleToAsciiText(bm2));
+		this.converter.asciiTextToImage(this.converter.grayScaleToAsciiText(bm2, this.activeFont.getLUT()));
 	}
 
 	public void manipulateImage(Bitmap bitmap) {
-		// TODO Auto-generated method stub
+		Bitmap bm2 = this.converter.bitmapToGrayScale(this.bm);
+		this.converter.asciiTextToImage(this.converter.grayScaleToAsciiText(bm2, this.activeFont.getLUT()));
 		
 	}
 
