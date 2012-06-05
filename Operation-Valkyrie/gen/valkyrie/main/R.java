fe1fb96824d4b096acdbd49d2c5b1b7eef9721fc
Laurenz Theuerkauf <offburn@gmx.at>
2012-06-05 15:48:28 +0200

    optical changes

--- Operation-Valkyrie/gen/valkyrie/main/R.java
+++ Operation-Valkyrie/gen/valkyrie/main/R.java
@@ -77,11 +77,13 @@ containing a value of this type.
         public static final int actionbar_back_transparent=0x7f020000;
         public static final int actionbar_backgorund=0x7f020001;
         public static final int camera_lens=0x7f020002;
-        public static final int gallery=0x7f020003;
-        public static final int ic_launcher=0x7f020004;
-        public static final int real_valkyrie=0x7f020005;
-        public static final int sliding_drawer_handle_left=0x7f020006;
-        public static final int valkyrie=0x7f020007;
+        public static final int filter=0x7f020003;
+        public static final int gallery=0x7f020004;
+        public static final int gallery_temp=0x7f020005;
+        public static final int ic_launcher=0x7f020006;
+        public static final int real_valkyrie=0x7f020007;
+        public static final int sliding_drawer_handle_left=0x7f020008;
+        public static final int valkyrie=0x7f020009;
     }
     public static final class id {
         public static final int action_bar=0x7f05000e;
