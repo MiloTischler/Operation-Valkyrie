750266dc58a4831ee6cb62bf04dc006a2fbd0e80
MiloTischler <milo.tischler@gmail.com>
2012-06-07 22:22:04 +0200

    Cleanup takePicture

--- Operation-Valkyrie/src/valkyrie/filter/ascii/Converter.java
+++ Operation-Valkyrie/src/valkyrie/filter/ascii/Converter.java
@@ -60,10 +60,10 @@ public class Converter {
 		
 		String textLine = new String();
 
+		//		Imgproc.cvtColor(gray, gray, Imgproc.COLOR_RGB2HSV, 4);
+		
 		Bitmap mybitmap = Bitmap.createBitmap(gray.width(), gray.height(), Bitmap.Config.RGB_565);
 		Imgproc.resize(gray, gray, new Size(width, height));
-		
-		Imgproc.cvtColor(gray, gray, Imgproc.COLOR_RGB2HSV, 4);
 		
 		Canvas canvas = new Canvas(mybitmap);
 		canvas.drawColor(Color.WHITE);
@@ -77,7 +77,7 @@ public class Converter {
 				textLine = "";
 				m = i;
 				n = j;
-				textLine += (char) LUT[(int) gray.get(n, m)[2]];
+				textLine += (char) LUT[(int) gray.get(n, m)[0]];
 				canvas.drawText(textLine, hightPos, widthPos, this.paint);
 
 				widthPos += this.fontsize;
