99b09c7c60c0e39e62308931d00df992284537e3
Laurenz Theuerkauf <offburn@gmx.at>
2012-06-10 19:37:52 +0200

    comments

--- Operation-Valkyrie/src/valkyrie/widget/TouchImageView.java
+++ Operation-Valkyrie/src/valkyrie/widget/TouchImageView.java
@@ -11,15 +11,11 @@ package valkyrie.widget;
 
 import android.content.Context;
 import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
 import android.graphics.Matrix;
 import android.graphics.PointF;
-import android.graphics.drawable.Drawable;
 import android.util.AttributeSet;
-import android.util.FloatMath;
 import android.util.Log;
 import android.view.GestureDetector;
-import android.view.GestureDetector.OnDoubleTapListener;
 import android.view.MotionEvent;
 import android.view.ScaleGestureDetector;
 import android.view.View;
