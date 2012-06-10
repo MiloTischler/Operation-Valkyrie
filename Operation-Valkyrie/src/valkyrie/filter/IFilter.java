18268212823e30daf4826c058345384782075289
MiloTischler <milo.tischler@gmail.com>
2012-06-10 18:46:16 +0200

    Commented valkyrie.filter package

--- Operation-Valkyrie/src/valkyrie/filter/IFilter.java
+++ Operation-Valkyrie/src/valkyrie/filter/IFilter.java
@@ -12,20 +12,58 @@ import android.widget.TableLayout;
  * © Milo Tischler, Jakob Schweighofer, Alexander Ritz, Paul Neuhold, Laurenz Theuerkauf
  * 
  */
+
+/**
+ * Filter Interface 
+ */
 public interface IFilter {
+	
+	/**
+	 * Setup methode, called at every programm start for filter intern initializations
+	 * 
+	 * @param FilterInternalStorage filterInternalStorage
+	 * @param FilterAssets filterAssets
+	 * @param Boolean firstRun
+	 */
 	public void setup(FilterInternalStorage filterInternalStorage, FilterAssets filterAssets, Boolean firstRun);
 
+	/**
+	 * Returns the short name of the filter
+	 * 
+	 * @return String name
+	 */
 	public String getName();
 
+	/**
+	 * Returns the icon of the filter
+	 * 
+	 * @return Bitmap icon
+	 */
 	public Bitmap getIcon();
 	
+	/**
+	 * Manipulates the image for the preview (Fast implementation!)
+	 * 
+	 * @param Mat bitmap
+	 * @return Bitmap, manipulated image
+	 */
 	public Bitmap manipulatePreviewImage(Mat bitmap);
 	
+	/**
+	 * Manipulates the image for taking a picture
+	 * 
+	 * @param Mat bitmap
+	 * @return Bitmap, manipulated image
+	 */
 	public Bitmap manipulateImage(Mat bitmap);
 	
+	/**
+	 * Returns the OpenCV video capture format the filter requests
+	 * 
+	 * @return int, OpenCV (see OpenCV Highgui class) video capture format the filter requests
+	 */
 	public int getFilterCaptureFormat();
 	
-	public void initOptions();
 	/**
 	 * Retrieves the filter specific options panel.
 	 * 
--- Operation-Valkyrie/src/valkyrie/filter/ascii/Ascii.java
+++ Operation-Valkyrie/src/valkyrie/filter/ascii/Ascii.java
@@ -159,11 +159,4 @@ public class Ascii implements IFilter {
 	public void setup(FilterInternalStorage filterInternalStorage, FilterAssets filterAssets, Boolean firstRun) {
 
 	}
-
-	/**
-	 * Initializes Options for the current Filter.
-	 */
-	public void initOptions() {
-
-	}
 }
