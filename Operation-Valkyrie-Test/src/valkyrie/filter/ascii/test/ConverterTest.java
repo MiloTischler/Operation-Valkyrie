6a96bb986a0bd60d302bd0b1855c0c84fb30d0c9
Aexander Ritz <alexander.ritz@gmx.at>
2012-06-11 21:43:41 +0200

    thats my last commit

---
+++ Operation-Valkyrie-Test/src/valkyrie/filter/ascii/test/ConverterTest.java
@@ -0,0 +1,50 @@
+package valkyrie.filter.ascii.test;
+
+import org.opencv.android.Utils;
+import org.opencv.core.Mat;
+
+import valkyrie.filter.ascii.Converter;
+import valkyrie.filter.ascii.Font;
+import valkyrie.ui.MainActivity;
+import android.graphics.BitmapFactory;
+import android.graphics.Bitmap;
+import android.test.ActivityInstrumentationTestCase2;
+import junit.framework.TestCase;
+
+public class ConverterTest extends ActivityInstrumentationTestCase2<MainActivity> {
+
+	private Mat mat;
+	private Bitmap input;
+	
+	public ConverterTest() {
+		super("valkyrie.ui", MainActivity.class);
+	}
+
+	protected void setUp() throws Exception {
+		super.setUp();
+		BitmapFactory.Options opts = new BitmapFactory.Options();
+		opts.inPreferredConfig = Bitmap.Config.RGB_565;
+		this.input = BitmapFactory.decodeResource(this.getActivity().getResources(), valkyrie.main.test.R.drawable.camera_test_pictrue);
+		this.mat = new Mat();
+		Utils.bitmapToMat(this.input, mat);
+	}
+	
+	public void testBWtoAscci() {
+		Converter con = new Converter();
+		Font font = new Font("test", true);
+		Bitmap ascii = con.grayscaleToASCII(this.mat, font.getLUT());
+		assertNotNull(ascii);
+		assertTrue((ascii.getHeight() <= this.input.getHeight()));
+		assertTrue((ascii.getWidth() <= this.input.getWidth()));
+	}
+	
+	public void testColortoAscci(){
+		Converter con = new Converter();
+		Font font = new Font("test", true);
+		Bitmap ascii = con.colorToASCII(this.mat, font.getLUT());
+		assertNotNull(ascii);
+		assertTrue((ascii.getHeight() <= this.input.getHeight()));
+		assertTrue((ascii.getWidth() <= this.input.getWidth()));
+	}
+
+}
